91467adde2d7e7bca479fe642dde6f554d301612
Sven Siepermann <sven.siepermann@tradingtwins.com>
2026-02-26 08:53:58 +0100

    forgot to change version

--- Formula/gitls.rb
+++ Formula/gitls.rb
@@ -1,7 +1,7 @@
 class Gitls < Formula
   desc "A fast, minimal tool to inspect and act on multiple git repositories"
   homepage "https://github.com/sven42xyz/gitools"
-  url "https://github.com/sven42xyz/gitools/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/sven42xyz/gitools/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "985305bb8d7a0ba1b7a93fbff591663f449030f02238f45de7a21a8b3cfb2f5c"
   license "MIT"
   head "https://github.com/sven42xyz/gitools.git", branch: "main"
